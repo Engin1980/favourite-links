be1ebf49e69809f3ed65d52b73a8a3bccf03f624
Marek Vajgl <engin@seznam.cz>
2025-09-13 15:35:23 +0200

    Initial commit, empty app

---
+++ favourite-links-be/src/main/java/cz/osu/kip/favouriteLinksBE/FavouriteLinksBeApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.kip.favouriteLinksBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteLinksBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouriteLinksBeApplication.class, args);
+	}
+
+}
---
+++ favourite-links-be/src/test/java/cz/osu/kip/favouriteLinksBE/FavouriteLinksBeApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.kip.favouriteLinksBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavouriteLinksBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
